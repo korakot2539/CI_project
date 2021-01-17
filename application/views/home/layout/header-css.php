4e89c3028e585f6a32334826f9fe7ff3b70e0b8b
cantidentify <55484784+cantidentify@users.noreply.github.com>
2021-01-17 13:56:38 +0700

    Add files via upload

---
+++ application/views/home/layout/header-css.php
@@ -0,0 +1,26 @@
+    
+	<!-- Font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600%7CUbuntu:300,400,500,700" rel="stylesheet"> 
+
+
+    
+    <!-- CSS -->
+    <?php echo link_tag('asset/home/css/bootstrap-reboot.min.css'); ?>
+
+    <?php echo link_tag('asset/home/css/bootstrap-grid.min.css'); ?>
+    <?php echo link_tag('asset/home/css/owl.carousel.min.css'); ?>
+    <?php echo link_tag('asset/home/css/jquery.mCustomScrollbar.min.css'); ?>
+    <?php echo link_tag('asset/home/css/nouislider.min.css'); ?>
+    <?php echo link_tag('asset/home/css/ionicons.min.css'); ?>
+    <?php echo link_tag('asset/home/css/plyr.css'); ?>
+    <?php echo link_tag('asset/home/css/photoswipe.css'); ?>
+    <?php echo link_tag('asset/home/css/default-skin.css'); ?>
+    <?php echo link_tag('asset/home/css/main.css'); ?>
+
+
+    <!-- Favicons -->
+    <?php echo link_tag('asset/home/icon/favicon-32x32.png'); ?>
+    <?php echo link_tag('asset/home/icon/favicon-32x32.png'); ?>
+    <?php echo link_tag('asset/home/icon/apple-touch-icon-72x72.png'); ?>
+    <?php echo link_tag('asset/home/icon/apple-touch-icon-114x114.png'); ?>
+    <?php echo link_tag('asset/home/icon/apple-touch-icon-144x144.png'); ?>
---
+++ application/views/home/layout/menu-top.php
@@ -0,0 +1,66 @@
+
+	<!-- header -->
+	<header class="header">
+		<div class="header__wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="header__content">
+							<!-- header logo -->
+							<a href="index.html" class="header__logo">
+								<img src="img/logo.svg" alt="">
+							</a>
+							<!-- end header logo -->
+
+							<!-- header nav -->
+							<ul class="header__nav">
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="#" role="button" aria-haspopup="true" aria-expanded="false">Home</a>
+								</li>
+
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="#" role="button" aria-haspopup="true" aria-expanded="false">History</a>
+								</li>
+
+								<li class="header__nav-item">
+									<a class="header__nav-link" href="#" role="button" aria-haspopup="true" aria-expanded="false">Contact</a>
+								</li>
+
+							</ul>
+							<!-- end header nav -->
+
+							<!-- header auth -->
+							<div class="header__auth">
+								<button class="header__search-btn" type="button">
+									<i class="icon ion-ios-search"></i>
+								</button>
+
+								<a href="signin.html" class="header__sign-in">
+									<i class="icon ion-ios-log-in"></i>
+									<span>sign in</span>
+								</a>
+							</div>
+							<!-- end header auth -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- header search -->
+		<form action="#" class="header__search">
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<div class="header__search-content">
+							<input type="text" placeholder="Search for a movie, TV Series that you are looking for">
+
+							<button type="button">search</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</form>
+		<!-- end header search -->
+	</header>
+	<!-- end header -->
