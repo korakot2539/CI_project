a61f8adae098bd3df8f95f4fb99d80f413eca95e
korakot <ph.korakot_st@tni.ac.th>
2021-02-26 02:18:42 +0700

    no message

--- application/views/home/layout/header-css.php
+++ application/views/home/layout/header-css.php
@@ -63,6 +63,26 @@
 	<link rel="apple-touch-icon" sizes="114x114" href="<? echo base_url() ?>sset/home/icon/apple-touch-icon-114x114.png">
 	<link rel="apple-touch-icon" sizes="144x144" href="<? echo base_url() ?>asset/home/icon/apple-touch-icon-144x144.png">
 
+	<!-- CSS -->
+    <link rel="stylesheet" href="<? echo base_url() ?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/nouislider.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/ionicons.min.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/plyr.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/photoswipe.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/default-skin.css">
+	<link rel="stylesheet" href="<? echo base_url() ?>asset/home/css/main.css">
+
+	<!-- Favicons -->
+	<link rel="icon" type="image/png" href="<? echo base_url() ?>asset/home/icon/favicon-32x32.png" sizes="32x32">
+	<link rel="apple-touch-icon" href="<? echo base_url() ?>asset/home/icon/favicon-32x32.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<? echo base_url() ?>asset/home/icon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<? echo base_url() ?>sset/home/icon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<? echo base_url() ?>asset/home/icon/apple-touch-icon-144x144.png">
+
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 	<meta name="author" content="Dmitry Volkov">
