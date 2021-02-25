1cc5a5affdd7cbfe67a1922607ce37ec7ac026d7
korakot <ph.korakot_st@tni.ac.th>
2021-02-26 03:39:01 +0700

    delete load in pdfgen

--- application/controllers/PDFgenerate.php
+++ application/controllers/PDFgenerate.php
@@ -5,8 +5,6 @@ class PDFgenerate extends CI_Controller {
 	function __construct()
 	{
         parent::__construct();
-        $this->load->database();
-        $this->load->helper("url");
     }
 
     function generate()
