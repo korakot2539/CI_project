991f568d4c634c5483c09ebb84573ffa726cee99
cantidentify <blackarmy94@gmail.com>
2021-03-04 20:52:49 +0700

    Fix active left menu, CSS, Pagination

--- admin/application/views/layout/menu-left.php
+++ admin/application/views/layout/menu-left.php
@@ -1,4 +1,4 @@
-<div class="sidebar" data="blue">
+<div class="sidebar" data="red">
     <div class="sidebar-wrapper">
         <div class="logo">
             <a href="javascript:void(0)" class="simple-text logo-mini">
@@ -10,19 +10,19 @@
             </div>
         </div>
         <ul class="nav">
-            <li class="active ">
+            <li class="<?php if($this->uri->segment(1)=="Movie"){echo "active";}?>">
                 <a href="<?php echo base_url('Movie') ?>">
                     <i class="tim-icons icon-video-66"></i>
                     <p class="leftpanel-text">Movie</p>
                 </a>
             </li>
-            <li>
+            <li class="<?php if($this->uri->segment(1)=="Category"){echo "active";}?>">
                 <a href="<?php echo base_url('Category') ?>">
                     <i class="tim-icons icon-tag"></i>
                     <p class="leftpanel-text">Category</p>
                 </a>
             </li>
-            <li>
+            <li class="<?php if($this->uri->segment(1)=="User"){echo "active";}?>">
                 <a href="<?php echo base_url('User') ?>">
                     <i class="tim-icons icon-single-02"></i>
                     <p class="leftpanel-text">User</p>
