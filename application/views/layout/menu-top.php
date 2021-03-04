9bbade5d4e9530725ff5ebbe04682acf647610d5
Thanapat <ch.thanapat_st@tni.ac.th>
2021-03-04 18:44:18 +0700

    Menu top

--- application/views/layout/menu-top.php
+++ application/views/layout/menu-top.php
@@ -8,7 +8,6 @@
 						<div class="header__content">
 							<!-- header logo -->
 							<a href=<?=base_url()?> class="header__logo">
-							<a href="index.html" class="header__logo">
 								<img src="<?php echo base_url('/asset/home/')?>img/logo.svg" alt="">
 							</a>
 							<!-- end header logo -->
@@ -43,23 +42,29 @@
 								<li class="header__nav-item">
 									<a class="header__nav-link" href="<?=base_url('Contact')?>" role="button" aria-haspopup="true" aria-expanded="false">Contact</a>
 								</li>
-
-								<div class="header__auth">
-								<button class="header__search-btn" type="button">
-									<i class="icon ion-ios-search"></i>
-								</button>
-
-
 							</ul>
+							<div class="header__auth">
+								<!-- <button class="header__search-btn" type="button">
+									<i class="icon ion-ios-search"></i>
+								</button> -->
+							</div>
+
 							<!-- end header nav -->
 							<!-- header auth -->
+							<div class="header__auth">
 								<a href=<?=base_url("Auth/loginForm")?> class="header__sign-in">
 								<i class="icon ion-ios-log-in"></i>
 									<span>login</span>
 								</a>
-
 							</div>
 							<!-- end header auth -->
+							<!-- header menu btn -->
+							<button class="header__btn" type="button">
+								<span></span>
+								<span></span>
+								<span></span>
+							</button>
+							<!-- end header menu btn -->
 						</div>
 					</div>
 				</div>
