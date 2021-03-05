a2bd7e8d7760622f68017af325a220eb5bcd3d9b
ibobbygithub <savesimulate@gmail.com>
2021-03-05 23:18:46 +0700

    update pdf

--- application/controllers/PDFgenerate.php
+++ application/controllers/PDFgenerate.php
@@ -5,8 +5,5 @@ class PDFgenerate extends CI_Controller
 
     function __construct()
     {
-        parent::__construct();
-        $this->load->helper(array('url'));
-        include(APPPATH . "third_party/mpdf/mpdf.php");
     }
 }
